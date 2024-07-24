f81e2be7fbf61c38726a1493e11be322fc3a1323
volkangumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-07-24 16:12:53 +0200

    Create get_daily_rain_data_from_knmi.py

---
+++ get_daily_rain_data_from_knmi.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import re
+import io
+import os
+import requests
+from requests.exceptions import HTTPError
+
+data = {
+    'start': '20140701',
+    'end': '20240701',
+    'vars': 'ALL',
+    'stns': '919',
+}
+
+URLS = ["https://daggegevens.knmi.nl/klimatologie/monv/reeksen"]
+
+for url in URLS:
+    try:
+        response = requests.post('https://www.daggegevens.knmi.nl/klimatologie/monv/reeksen', data=data)
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f"HTTP error occurred : {http_err}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+    else:
+        print("Success with status code",response.status_code)
+
+##Parse response as df
+
+s=str(response.content)# response as string
+s=s[s.rindex('#')+1:] #find last # and delete the first part
+s=s.replace('\\n','\n') #replace //n with /n
+s=s.replace(' ','') #delete space
+s=s.replace("'",'') #delete '
+df=pd.read_csv(io.StringIO(s), sep=",")
+type(df)
+print(df.dtypes)
+print(df.shape)
+
+df.to_csv('daily_rain_data.csv',index=False)
