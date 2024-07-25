08081e5fed1c95de8cfeac201dd23ac1e77e5dc9
volkangumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-07-25 19:49:42 +0200

    corrected echo statement

--- set_dates_for_training.py
+++ set_dates_for_training.py
@@ -8,14 +8,14 @@ end_date=int(datetime.date.today().strftime('%Y%m%d'))-7
 start_date=int(end_date)-100000
 
 var_name='TRAIN_START_DATE'
-var_value="start_date"
+var_value=str(start_date)
 echo_statement="echo "+"\""+var_name+"="+var_value+"\""+" >> $GITHUB_ENV"
 print("sending the following to os.system")
 print(echo_statement)
 os.system(echo_statement)
 
 var_name='TRAIN_END_DATE'
-var_value="end_date"
+var_value=str(end_date)
 echo_statement="echo "+"\""+var_name+"="+var_value+"\""+" >> $GITHUB_ENV"
 print("Sending the following to os.system")
 print(echo_statement)
