1a1564409b90a667f3d099b198c659f4524f0e57
volkangumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-07-25 19:31:07 +0200

    Create set_dates for training.py

---
+++ set_dates for training.py
@@ -0,0 +1,21 @@
+import datetime
+
+#set training end date to 1 week ago
+end_date=int(datetime.date.today().strftime('%Y%m%d'))-7
+
+#start_date is 10 years before end_date
+start_date=int(end_date)-100000
+
+var_name='TRAIN_START_DATE'
+var_value="start_date"
+echo_statement="echo "+"\""+var_name+"="+var_value+"\""+" >> $GITHUB_ENV"
+print("sending the following to os.system")
+print(echo_statement)
+os.system(echo_statement)
+
+var_name='TRAIN_END_DATE'
+var_value="end_date"
+echo_statement="echo "+"\""+var_name+"="+var_value+"\""+" >> $GITHUB_ENV"
+print("Sending the following to os.system")
+print(echo_statement)
+os.system(echo_statement)
