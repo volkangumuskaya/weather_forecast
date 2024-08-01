a04b4846e547c4f0e8254607710952eaf965a00b
volkangumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2024-08-01 12:22:40 +0200

    Update set_dates_for_training.py

--- set_dates_for_training.py
+++ set_dates_for_training.py
@@ -2,16 +2,21 @@ import datetime
 import os
 
 #set training end date to 1 week ago
-end_date=int(datetime.date.today().strftime('%Y%m%d'))-7
+end_date=int((datetime.date.today()-datetime.timedelta(days=7)).strftime('%Y%m%d'))
 
 #go back one month because for some reason knmi website does not provide the current month data
 #since this is just to try github action capabilities, and not make real time predcitions that will be used, it s ok
 #better than causing data leakage 
 #it will simulate how system would work one month ago
-end_date=end_date-100
+end_date=(datetime.date.today()-datetime.timedelta(days=37))
 
 #start_date is 10 years before end_date
-start_date=int(end_date)-100000
+start_date=end_date-datetime.timedelta(days=3650)
+
+end_date=int(end_date.strftime('%Y%m%d'))
+start_date=int(start_date.strftime('%Y%m%d'))
+
+#note that here there are complications due to leap year, months that are not 30 days etc but it is not so significant for the purposes of this project
 
 var_name='TRAIN_START_DATE'
 var_value=str(start_date)
